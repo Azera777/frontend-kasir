025baa857c56b648b843e34fd3bbdd105e765d1e
Azera <cintaazera072@gmail.com>
2024-09-19 11:48:02 +0700

    update cart (not done)

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,13 +1,12 @@
-import React, { useContext } from 'react'
-import './Cart.css'
-import { StoreContext } from '../../context/StoreContext'
+import React, { useContext } from "react";
+import "./Cart.css";
+import { StoreContext } from "../../context/StoreContext";
 
 const Cart = () => {
-
-  const {cartItems,food_list,removefromCart} = useContext(StoreContext);
+  const { cartItems, food_list, removeFromCart } = useContext(StoreContext);
 
   return (
-    <div className='cart'>
+    <div className="cart">
       <div className="cart-items">
         <div className="cart-items-title">
           <p>Items</p>
@@ -17,9 +16,52 @@ const Cart = () => {
           <p>Total</p>
           <p>Remove</p>
         </div>
+        <br />
+        <hr />
+        {food_list.map((item, index) => {
+          if (cartItems[item.id] > 0) {
+            return (
+              <div>
+                <div className="cart-items-title-card-items-item">
+                  <img src={item.image} alt="" />
+                  <p>{item.name}</p>
+                  <p>{item.price}</p>
+                  <p>{cartItems[item.id]}</p>
+                  <p>{item.price * cartItems[item.id]}</p>
+                  <p onClick={()=>removeFromCart(item.id)}>x</p>
+                </div>
+                <hr />
+              </div>
+            );
+          }
+        })}
+      </div>
+      <div>
+        <div className="cart-bottom">
+          <div className="cart-total">
+            <h2>Cart Total</h2>
+            <div>
+              <div className="cart-total-details">
+                <p>Subtotal</p>
+                <p>{0}</p>
+              </div>
+              <hr />
+              <div className="cart-total-details">
+                <p>Delivery Fee</p>
+                <p>{2}</p>
+              </div>
+              <hr />
+              <div className="cart-total-details">
+                <p>Total</p>
+                <p>{0}</p>
+              </div>
+              <button>Checkout</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
